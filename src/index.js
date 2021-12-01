24baf36d6bd9ccda6df086acf160bf5b7d787fc3
mathunes <matheusantunes720@gmail.com>
2021-12-01 17:58:18 -0300

    Add create user and todos function

--- src/index.js
+++ src/index.js
@@ -1,29 +1,83 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const username = request.get("username");
+  
+  const user = users.find(
+    user => user.username === username
+  );
+
+  if (!user) {
+    return response.status(400).json({ error: "User not found" });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+
+  const userAlreadyExists = users.some(
+    (users) => users.username === username
+  )
+
+  if (userAlreadyExists) {
+    return response.status(400).json({error: "User already exists"});
+  }
+
+  const user = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+  }
+  
+  users.push(user);
+
+  return response.status(200).send({name, username})
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const username = request.get("username");
+
+  const user = users.find(
+    user => user.username === username
+  );
+
+  const todos = user.todos;
+
+  return response.status(200).json(todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body;
+
+  const { user } = request;
+
+  const todo = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date()
+  }
+
+  user.todos.push(todo);
+
+  return response.status(200).json(user.todos);
+
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
